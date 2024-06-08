f9e944cc068d03bb25de4d8bd6d190a58bd82d9d
991 <76601560+tomascapo991@users.noreply.github.com>
2024-06-08 19:25:11 -0300

    Add files via upload
    Primer modelo del portafolio.

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function toggleDescription(projectElement) {
+    const description = projectElement.querySelector('.description');
+    if (description.style.display === 'none' || description.style.display === '') {
+        description.style.display = 'block';
+    } else {
+        description.style.display = 'none';
+    }
+}
